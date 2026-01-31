b18f3c703b74ad8b6202f9084024792c6300c6c0
Alim <a_shimarov@kbtu.kz>
2026-01-31 12:52:17 +0500

    Initial commit

---
+++ Variables.py
@@ -0,0 +1,34 @@
+x = 1       # x is of type int
+x = "kbtu" # x is now of type str
+print(x)
+
+languages = ["python", "c++", "c#"]
+x, y, z = languages
+print(x)
+print(y)
+print(z)
+
+x = "a "
+y = "b "
+z = "c"
+print(x + y + z)
+
+
+ x = "Alim"
+
+def myfunc():
+  global x
+  x = "Dimash"
+
+myfunc()
+
+print("My name is " + x)
+
+
+x = "Alim"
+
+def myfunc():
+  print("Pythmy name is " + x)
+
+myfunc()
+
