e5937d46b153fa69d817fb07d973c1545fe0c25c
Alim <a_shimarov@kbtu.kz>
2026-02-21 13:32:31 +0500

    Remove duplicate classes and inheritance folders

--- classes/class_definition.py
+++
@@ -1,6 +0,0 @@
-class Car:
-    pass 
-
-car1 = Car() 
-
-print("Car object created:", car1)
--- classes/init_method.py
+++
@@ -1,8 +0,0 @@
-class Student:
-    def __init__(self, name, age):
-        self.name = name      
-        self.age = age       
-
-student1 = Student("Aruzhan", 18)
-print("Name:", student1.name)
-print("Age:", student1.age)
